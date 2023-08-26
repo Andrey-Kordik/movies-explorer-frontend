113396b9becc8aa7aa6f11c6a5628f949d9cdffc
Andrey Kordik <andreykordik@yandex.ru>
2023-08-26 17:19:33 +0400

    level-3

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,55 +2,56 @@
 import './MoviesCardList.css';
 import MovieCard from '../MoviesCard/MovieCard';
 
-function MoviesCardList({ movies, 
-  onAddMovie, 
-  onDeleteMovie, 
-  isSubmitted, 
-  loadMoreMovies, 
-  showButton, 
-  displayedCardsDesktop,
-  displayedCardsMobile, 
-  isMoviesRoute }) {
-
-  return (
-    <section className="movies__list">
-      <div className="movies__container">
-        {movies.length > 0 ? (
-          isMoviesRoute ? (
-            isSubmitted &&
-            movies
-              .slice(0, displayedCardsDesktop >= displayedCardsMobile ? displayedCardsDesktop : displayedCardsMobile)
-              .map((movie) => (
-                <MovieCard
-                  key={movie.nameRU}
-                  movie={movie}
-                  onAddMovie={onAddMovie}
-                  onDeleteMovie={onDeleteMovie}
-                />
-              ))
-          ) : (
-            movies.map((movie) => (
-              <MovieCard
-                key={movie.nameRU}
-                movie={movie}
-                onAddMovie={onAddMovie}
-                onDeleteMovie={onDeleteMovie}
-              />
-            ))
-          )
-        ) : (
-          isSubmitted && movies.length === 0 ? (
-            <p className="movies__not-found">Ничего не найдено</p>
-          ) : []
-        )}
-      </div>
-      {showButton && displayedCardsDesktop + displayedCardsMobile < movies.length && (
-        <button className="movies__more-button" onClick={loadMoreMovies}>
-          Ещё
-        </button>
-      )}
-    </section>
-  );
+function MoviesCardList({ movies,   
+  onAddMovie,   
+  onDeleteMovie,   
+  isSubmitted,   
+  loadMoreMovies,   
+  showButton,   
+  displayedCardsDesktop,  
+  displayedCardsMobile,   
+  isMoviesRoute }) {  
+  
+  return (  
+    <section className="movies__list">  
+      <div className="movies__container">  
+        {movies.length > 0 ? (  
+          isMoviesRoute ? (  
+            isSubmitted &&  
+            movies  
+              .slice(0, displayedCardsDesktop >= displayedCardsMobile ? displayedCardsDesktop : displayedCardsMobile)  
+              .map((movie) => (  
+                <MovieCard  
+                  key={movie.nameRU}  
+                  movie={movie}  
+                  onAddMovie={onAddMovie}  
+                  onDeleteMovie={onDeleteMovie}  
+                />  
+              ))  
+          ) : (  
+            movies.map((movie) => (  
+              <MovieCard  
+                key={movie.nameRU}  
+                movie={movie}  
+                onAddMovie={onAddMovie}  
+                onDeleteMovie={onDeleteMovie}  
+              />  
+            ))  
+          )  
+        ) : (  
+          (isSubmitted && movies.length === 0) || (!isMoviesRoute && movies.length === 0) ? (  
+            <p className="movies__not-found">Ничего не найдено</p>  
+          ) : null  
+        )}  
+      </div>  
+      {showButton && displayedCardsDesktop + displayedCardsMobile < movies.length && (  
+        <button className="movies__more-button" onClick={loadMoreMovies}>  
+          Ещё  
+        </button>  
+      )}  
+    </section>  
+  );  
 }
+
 
 export default MoviesCardList;
